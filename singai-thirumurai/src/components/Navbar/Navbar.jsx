9adfdfa8376f1108bc285f5f2438b8a875a52821
Chockalingam Kasi <chocka1998@gmail.com>
2024-06-25 22:14:26 +0800

    first commit

---
+++ singai-thirumurai/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className='container'>
+      <img src={logo} alt="" className='logo'/>
+      <ul>
+        <li>Home</li>
+        <li>Events</li>
+        <li>About Us</li>
+        <li>Lessons</li>
+        <li>Resources</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
