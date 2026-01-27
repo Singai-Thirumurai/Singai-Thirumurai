5b2a0ce592223c501ec40aba03449b9861d21dbe
SViswa02 <sviswa2002@gmail.com>
2026-01-27 23:58:10 +0800

    Edit modals with data

--- singai-thirumurai/src/components/Events/Events.jsx
+++ singai-thirumurai/src/components/Events/Events.jsx
@@ -5,12 +5,14 @@ import event_1 from '../../assets/manadu.jpg'
 import event_2 from '../../assets/competition.jpg'
 import event_3 from '../../assets/thevaaram_class_placeholder.jpg'
 import event_4 from '../../assets/placeholder-img.jpg'
+import event_5 from '../../assets/youthwing.jpg'
 
 import program_icon_1 from '../../assets/program-icon-1.png'
 import program_icon_2 from '../../assets/program-icon-2.png'
 import program_icon_3 from '../../assets/program-icon-3.png'
 import program_icon_4 from '../../assets/program-icon-4.png'
 import program_icon_5 from '../../assets/program-icon-5.png'
+import instagram_icon from '../../assets/instagram_code.png'
 
 const Events = () => {
     const [modalType, setModalType] = useState(null); // 'classes' or 'competitions' or 'manadu' or 'payirchi-kazhagam' or null
@@ -32,6 +34,19 @@ const Events = () => {
         { name: "Sri Maha Mariamman Temple", zone: "North" },
         { name: "Sri Velmurugal Gnanamuneeswaran Temple", zone: "North/East" },
         { name: "Mrs Savithri Karthikeyan c/o Sri Muneeswarar Temple", zone: "West" }
+    ];
+
+    const dramaData = [
+        { title: "பித்தா பிறை சூடி", year: 2014, link: "https://www.youtube.com" },
+        { title: "திருநீலகண்டம்", year: 2015, link: "https://www.youtube.com" },
+        { title: "திலகவதியார் நாடகம்", year: 2016, link: "https://www.youtube.com" },
+        { title: "அப்பூதி அடியார் நாடகம்", year: 2017, link: "https://www.youtube.com" },
+        { title: "நற்றமிழ் ஞானசம்பந்தர் நாடகம்", year: 2018, link: "https://www.youtube.com" },
+        { title: "கண்ணப்பர் நாயனார் நாடகம்", year: 2019, link: "https://www.youtube.com" },
+        { title: "திருமுறை கண்ட புராணம் நாடகம்", year: 2022, link: "https://www.youtube.com" },
+        { title: "காரைக்கால் அம்மையார் நாடகம்", year: 2023, link: "https://www.youtube.com" },
+        { title: "மாணிக்கவாசகர் நாடகம்", year: 2024, link: "https://www.youtube.com" },
+        { title: "சேரமான் பெருமாள் நாடகம்", year: 2025, link: "https://www.youtube.com" }
     ];
 
     return (
@@ -68,14 +83,14 @@ const Events = () => {
 
             <div className='event'>
                 <img src={event_4} alt="" />
-                <div className="caption" onClick={() => setModalType('merch')}>
+                <div className="caption" onClick={() => setModalType('drama')}>
                     <img src={program_icon_4} alt="" />
-                    <p>Thirumurai Merchandise</p>
+                    <p>Thirumurai Drama</p>
                 </div>
             </div>
 
             <div className='event'>
-                <img src={event_4} alt="" />
+                <img src={event_5} alt="" />
                 <div className="caption" onClick={() => setModalType('thirumurai-youth-wing')}>
                     <img src={program_icon_5} alt="" />
                     <p>Thirumurai Youth Wing</p>
@@ -135,20 +150,48 @@ const Events = () => {
             </Modal>
 
             <Modal
-                isOpen={modalType === 'merch'}
+                isOpen={modalType === 'drama'}
                 onClose={() => setModalType(null)}
-                title="Thirumurai Merchandise"
+                title="Thirumurai Drama"
             >
-                <p>This is a where you can find our merch! Keep a look out at this space for something fun in the future!</p>
+                <p>This is a where you can find out more about the various nayanmaar dramas that we have hosted thus far.</p>
+                <table className='temple-table'>
+                    <thead>
+                        <tr>
+                            <th>Drama Name</th>
+                            <th>Year</th>
+                            <th>Link</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {dramaData.map((drama, index) => (
+                            <tr key={index}>
+                                <td>{drama.title}</td>
+                                <td>{drama.year}</td>
+                                <td><a href={drama.link} target="_blank" rel="noopener noreferrer">Link here!</a></td> 
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </Modal>
 
-            
+
             <Modal
                 isOpen={modalType === 'thirumurai-youth-wing'}
                 onClose={() => setModalType(null)}
                 title="Thirumurai Youth Wing"
             >
-                <p>Thirumurai Youth Wing is a group of young people who are passionate about the Thirumurai tradition and want to contribute to the community...</p>
+                <p>We are a group of teens and young adults who are passionate about sharing the beauty of Tamil devotional music, knows as thevaram.
+                    We also indulge in volunteering programs as team and hope to spread positivity and deep spirituality in youths like us.</p>
+                    <br />
+                <p><strong>We organise Thirumurai Camps (yearly), take part in Sivarathiri, Navarathri and many other events!</strong></p>
+                <br />
+                <p>Join us on our journey to share the beauty of Tamil devotional music and make a positive impact in our community!
+                    You may contact us via our Instagram page below or contact our email (thirumuraiyouthwing@gmail.com) for more information.</p>
+                <div style={{ textAlign: 'center' }}>
+                    <img className="instagram-icon" src={instagram_icon}></img>
+                    <a href='https://www.instagram.com/singaithirumurai_sg?utm_source=qr&igsh=MTVzYm1wNWlkYzEzZA==' target='_blank' rel='noopener noreferrer'>Follow us on Instagram!</a>
+                </div>
             </Modal>
         </div>
     )
--- singai-thirumurai/src/components/Footer/Footer.jsx
+++ singai-thirumurai/src/components/Footer/Footer.jsx
@@ -1,31 +1,49 @@
 import React, { useState } from 'react';
 import Modal from '../Modal/Modal';
 import './Footer.css';
+import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
   const [modalType, setModalType] = useState(null); // 'privacy' or 'terms' or null
 
   return (
     <div className='footer'>
-      <p>© 2026 Singapore Thirumurai Manadu. All rights reserved.</p>
+
+      <div className="footer-content">
+        <p>© 2026 Singapore Thirumurai Manadu. All rights reserved.</p>
+
+        {/* Social Media Icons Section */}
+        <div className="social-icons">
+          <a href="https://www.facebook.com/sivayanama.thirumurai" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+            <FaFacebook className="social-icon" />
+          </a>
+          <a href="https://www.instagram.com/singaithirumurai_sg?utm_source=qr&igsh=MTVzYm1wNWlkYzEzZA==" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+            <FaInstagram className="social-icon" />
+          </a>
+          <a href="https://youtube.com/@sivayanamathirumurai6130?si=u0BKNoCF2IQXdwzN" target="_blank" rel="noopener noreferrer" aria-label="Youtube">
+            <FaYoutube className="social-icon" />
+          </a>
+        </div>
+      </div>
+
       <ul>
         <li onClick={() => setModalType('terms')}>Terms of Service</li>
         <li onClick={() => setModalType('privacy')}>Privacy Policy</li>
       </ul>
 
       {/* Terms of Service Modal */}
-      <Modal 
-        isOpen={modalType === 'terms'} 
-        onClose={() => setModalType(null)} 
+      <Modal
+        isOpen={modalType === 'terms'}
+        onClose={() => setModalType(null)}
         title="Terms of Service"
       >
         <p>Your terms of service content goes here...</p>
       </Modal>
 
       {/* Privacy Policy Modal */}
-      <Modal 
-        isOpen={modalType === 'privacy'} 
-        onClose={() => setModalType(null)} 
+      <Modal
+        isOpen={modalType === 'privacy'}
+        onClose={() => setModalType(null)}
         title="Privacy Policy"
       >
         <p>Your privacy policy content goes here...</p>
